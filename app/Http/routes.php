ff2482b596d241028fc84a24d751024b17fa3c0e
Achala Dias <diaspositive@gmail.comcol>
2016-02-23 20:36:19 +0530

    Achala
    Route page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+
+session_start();
+
+use Illuminate\Support\Facades\Input;
 /*
 |--------------------------------------------------------------------------
 | Routes File
@@ -10,44 +14,174 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+/* ////////////////////////     unicon_Admin Routes    /////////////////////// */
 
-session_start();
+Route::get('unicon_admin_index1', function () {
+    return view('unicon_admin.index')->with('title','Dashboard');
+});
 
-use Illuminate\Support\Facades\Input;
-
-
-/* ////////////////////////    Admin Routes    /////////////////////// */
-
-//Redirect to Dashboard page 
 Route::get('unicon_admin_index', 'Unicode_UserController@trackTotalUsers');
 
-//Redirect to View user page 
+
 Route::get('unicon_admin_view_users', 'Unicode_UserController@fillUserTable');
 
-/* ////////////////////////    END OF Admin Routes    /////////////////////// */
+
+
+
+/* ////////////////////////     Admin Routes    /////////////////////// */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('uni_index', function () {
+    return view('adminPage.uni_index');
+});
+
+Route::get('tables', function () {
+    return view('adminPanel.tables');
+});
+
+Route::get('forms', function () {
+    return view('adminPanel.forms');
+});
+
+Route::get('charts', function () {
+    return view('adminPanel.charts');
+});
+
+Route::get('grid', function () {
+    return view('adminPanel.bootstrap-grid');
+});
+
+Route::get('elements', function () {
+    return view('adminPanel.bootstrap-elements');
+});
+
+Route::get('blank', function () {
+    return view('adminPanel.blank-page');
+});
+
+Route::get('index-rtl', function () {
+    return view('adminPanel.index-rtl');
+});
+
+/* ---------------------------------------------------------------------*/
+
+/* ////////////////////////     Unicon Admin Routes    /////////////////////// */
+
+Route::get('unicon_index', function () {
+    return view('adminPage.index');
+});
+
+Route::get('unicon_index_2', function () {
+    return view('adminPage.index-2');
+});
+
+Route::get('unicon_button', function () {
+    return view('adminPage.buttons');
+});
+
+Route::get('unicon_charts', function () {
+    return view('adminPage.charts');
+});
+
+Route::get('unicon_calendar', function () {
+    return view('adminPage.calendar');
+});
+
+Route::get('unicon_chat', function () {
+    return view('adminPage.chat');
+});
+
+Route::get('unicon_form_common', function () {
+    return view('adminPage.form-common');
+});
+
+Route::get('unicon_validation', function () {
+    return view('adminPage.form-validation');
+});
+
+Route::get('unicon_wizard', function () {
+    return view('adminPage.form-wizard');
+});
+
+Route::get('unicon_gallery', function () {
+    return view('adminPage.gallery');
+});
+
+Route::get('unicon_grid', function () {
+    return view('adminPage.grid');
+});
+
+Route::get('unicon_interface', function () {
+    return view('adminPage.interface');
+});
+
+Route::get('unicon_invoice', function () {
+    return view('adminPage.invoice');
+});
+
+Route::get('unicon_jquery_ui', function () {
+    return view('adminPage.jquery-ui');
+});
+
+Route::get('unicon_login', function () {
+    return view('adminPage.login');
+});
+
+Route::get('unicon_message', function () {
+    return view('adminPage.message');
+});
+
+Route::get('unicon_tables', function () {
+    return view('adminPage.tables');
+});
+
+Route::get('unicon_widgets', function () {
+    return view('adminPage.widgets');
+});
+
+/* ---------------------------------------------------------------------*/
+
+
+
+/* ////////////////////////     Kids Routes     /////////////////////// */
+
+Route::get('kids_index', function () {
+    return view('kidsPages.index');
+});
+
+Route::get('kids_about', function () {
+    return view('kidsPages.about');
+});
+
+Route::get('kids_events', function () {
+    return view('kidsPages.events');
+});
+
+Route::get('kids_gallery', function () {
+    return view('kidsPages.gallery');
+});
+/* ---------------------------------------------------------------------*/
 
 
 
 
 
-/* ////////////////////////    Kids Routes    /////////////////////// */
-
-//Redirect to quiz page 
-Route::get('kids_quiz', 'Kids_QuizController@createQuiz');
-
-//Save exam results to 'exam_result' table
-Route::get('answer', 'Kids_QuizController@answer');
-
-//Redirect to certificate page 
-Route::get('kids_certificate', 'Kids_QuizController@certificate');
-
-/* ////////////////////////    END OF Kids Routes    /////////////////////// */
 
 
 
 
 
-/* ////////////////////////    Parent Routes    /////////////////////// */
+
+
+
+
+//Parent Account routes
+
+
+
 
 Route::get('login',function(){
     return view('login');
@@ -108,19 +242,11 @@ Route::get('reset_password_ajax','RegistrationController@reset_password');
 Route::get('reset_password_intellikid_2016_encript_version', 'EmailController@emailReset');
 Route::get('set_new_password','RegistrationController@set_new_password');
 
-<<<<<<< HEAD
 
 
 
 //end of parent Accounts routes
 
-=======
-    $id =  $_REQUEST['id'];
-   
-    $result = DB::select("select a.reset_token from parent_registration a where a.user_id = '$id'");
-
-    DB::table('parent_registration')->where('user_id', $id)->update(array('status' => 'ACTIVE'));
->>>>>>> origin/Main_Branch
 
 //child Account handing
 
@@ -159,32 +285,15 @@ Route::get('videos',function(){
 
 
 
-<<<<<<< HEAD
-
-=======
-     $email = $_SESSION['EMAIL'];
-     $token = $_REQUEST['token'];
-     //$_SESSION['TOKEN'] = "0";
-     $result = DB::select("select a.user_id,a.reset_token from parent_registration a where a.email = '$email' ");
->>>>>>> origin/Main_Branch
 
 
-<<<<<<< HEAD
-=======
-        return view('login');
-     }else{
-        $_SESSION['TOKEN'] = $token;
-        return view('reset_password');
-     }
-}); 
->>>>>>> origin/Main_Branch
+
 
 
 Route::get('delete_col_v',function(){
     
  $id = $_REQUEST['id'];
 
-<<<<<<< HEAD
  $pid = $_SESSION['USERID'] ;
 
  DB::statement(DB::raw(
@@ -252,56 +361,7 @@ Route::get('delete_from_collector_video','ParentVideosContoller@delete_from_coll
 Route::get('add_to_collector_video','ParentVideosContoller@add_to_collector_video');
 
 //end of the child acoount handing
-=======
-/* ////////////////////////    END OF Parent Routes    /////////////////////// */
 
-
-
-
-/* ////////////////////////     Upload Routes     /////////////////////// */
-Route::get('upload_videos', 'UploadVideoController@loadWithDefaultData');
-
-Route::get('upload_videos_fileError', 'UploadVideoController@loadWithFailedReason');
-
-Route::post('upload_videos123', 'BaseUploadController@uploadContent');
-
-Route::get('upload_videos123', 'UploadVideoController@loadWithDefaultData');
-
-
-
-
-Route::get('upload_songs', 'UploadSongController@loadWithDefaultData');
-
-Route::get('upload_songs_fileError', 'UploadSongController@loadWithFailedReason');
-
-Route::post('upload_songs123', 'BaseUploadController@uploadContent');
-
-Route::get('upload_songs123', 'UploadSongController@loadWithDefaultData');
->>>>>>> origin/Main_Branch
-
-
-
-Route::get('upload_story', 'UploadStoryController@loadWithDefaultData');
-
-Route::get('upload_story_fileError', 'UploadstoryController@loadWithFailedReason');
-
-Route::post('upload_story123', 'BaseUploadController@uploadContent');
-
-Route::get('upload_story123', 'UploadstoryController@loadWithDefaultData');
-
-Route::get('refresh', 'UploadSongController@loadWithSelectedCrit');
-
-
-
-
-Route::get('upload_question', 'UploadQuestionController@loadWithDefaultData');
-
-Route::post('upload_question123', 'BaseUploadController@uploadContent');
-
-Route::get('test', 'Test@testing');
-
-Route::post('test', 'Test@testing');
-/* ---------------------------------------------------------------------*/
 
 
 
